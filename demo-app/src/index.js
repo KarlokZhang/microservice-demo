0e0e904893531ac5526f3fc344f161c2c2a2b4c9
KarlokCheung <georgecheung07@gmail.com>
2021-09-01 21:54:37 +1000

    add session redux

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -2,15 +2,16 @@ import '@babel/polyfill';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 import graphqlClient from '#root/api/graphqlClient';
 import Root from '#root/components/Root';
 
 import * as theme from './theme';
+import store from './store';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
   html, body, #app {
     height: 100%;
@@ -24,11 +25,13 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 render(
-  <ApolloProvider client={graphqlClient}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Root />
-    </ThemeProvider>
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={graphqlClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Root />
+      </ThemeProvider>
+    </ApolloProvider>
+  </Provider>,
   document.getElementById('app'),
 );
